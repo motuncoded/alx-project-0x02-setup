40c04758914e7fcce2af130cea0dea3b90df47a5
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-02 18:50:34 +0100

    task 1 done

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        Welcome
+        </main>
+    </div>
+  );
+}
