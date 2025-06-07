5df13e8eb5c0ea3b098008cb04326e575f077c07
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-06-07 06:38:22 +0100

    task 5

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {  ButtonProps } from "@/interfaces";
+import { type ButtonProps } from "@/interfaces";
 
-const sizeStyles = {
+const sizeStyles: Record<string, React.CSSProperties> = {
   small: { padding: "4px 12px", fontSize: 12 },
   medium: { padding: "8px 20px", fontSize: 16 },
   large: { padding: "12px 28px", fontSize: 20 },
 };
 
-const shapeStyles = {
+const shapeStyles: Record<string, React.CSSProperties> = {
   "rounded-sm": { borderRadius: 4 },
   "rounded-md": { borderRadius: 12 },
   "rounded-full": { borderRadius: 9999 },
 };
 
-const Button = ({
+const Button: React.FC<ButtonProps> = ({
   children,
   size = "medium",
   shape = "rounded-md",
